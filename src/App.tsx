a56f6982a00d25f83fd771abe2d4d60ddaaedf3b
vladplesu <vladp1987@gmail.com>
2021-06-10 00:12:42 +0300

    add search input

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { InputBase, List, ListItem, ListItemText } from '@material-ui/core';
+import { StockSymbol } from './reducer';
 import './styles/theme.scss';
 
+const { REACT_APP_FIN_URL: URL, REACT_APP_FIN_KEY: KEY } = process.env;
+const SEARCH_URL = `${URL}/search?token=${KEY}`;
+
 function App() {
+  const [symbols, setSymbols] = useState([]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    fetch(`${SEARCH_URL}&q=${event.target.value}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setSymbols(data.result);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="App">
-      Add app here.
+      <InputBase placeholder="Search..." onChange={handleChange} />
+      <List dense>
+        {symbols.map((s: StockSymbol) => (
+          <ListItem key={s.symbol}>
+            <ListItemText primary={s.symbol} />
+          </ListItem>
+        ))}
+      </List>
     </div>
   );
 }
